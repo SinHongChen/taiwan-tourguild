837bbca33ba171eabd82567b2af6e62388f368b2
SinHongChen <a09342712201@gmail.com>
2021-11-07 16:57:41 +0800

    刪除非必要的圖片跟檔案

--- src/components/hub/LayoutHub.js
+++
@@ -1,9 +0,0 @@
-import BasicLayout from "components/layout/BasicLayout";
-import { device } from "components/layout/device";
-import ScrollTop from "components/layout/ScrollTop";
-
-export {
-    BasicLayout,
-    device,
-    ScrollTop
-}
